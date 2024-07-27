036077c8f7b5526d103601c5bcd9a11566cba078
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-07-27 13:58:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const botsConfig = [
     },
     {
         token: '6808636294:AAHSrYD-xN9OBYBKECGsrfe7YOVBtUXVnZk',
-        url: 'https://t.me/+19xZZx2z91M2ZDM0',
+        url: 'https://t.me/+0sksMowhhacxYTdk',
         savePath: 'https://solkah.org/melbet/index3.php',
         dataPath: 'https://solkah.org/melbet/id/data3.txt',
         adminId: 814566054
